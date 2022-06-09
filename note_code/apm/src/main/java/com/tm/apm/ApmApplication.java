a49c9301d44a640e8d270aca29d8c6a58cef97f9
tangming <infly123@sina.com>
2022-06-09 10:08:36 +0800

    update

--- note_code/apm/src/main/java/com/tm/apm/ApmApplication.java
+++ note_code/apm/src/main/java/com/tm/apm/ApmApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.tm.apm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class ApmApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(ApmApplication.class, args);
 	}
 
 }
--- note_code/apm/src/test/java/com/tm/apm/ApmApplicationTests.java
+++ note_code/apm/src/test/java/com/tm/apm/ApmApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.tm.apm;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class ApmApplicationTests {
 
 	@Test
 	void contextLoads() {
