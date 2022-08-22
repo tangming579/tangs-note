81f56e8ef0d12f54e3c614b2a4a8e9c7a361f5ec
汤明 <infly123@sina.com>
2022-08-22 11:26:58 +0800

    国密

--- note_code/gm/src/test/java/com/tm/gm/ApplicationTests.java
+++ note_code/gm/src/test/java/com/tm/gm/ApplicationTests.java
@@ -6,6 +6,7 @@ import com.auth0.jwt.interfaces.DecodedJWT;
 import com.auth0.jwt.interfaces.JWTVerifier;
 import com.tm.gm.common.gm.JwtHelper;
 import com.tm.gm.common.gm.SMAlgorithm;
+import com.tm.gm.common.utils.BCECUtil;
 import com.tm.gm.common.utils.SM2Util;
 import org.bouncycastle.crypto.AsymmetricCipherKeyPair;
 import org.bouncycastle.crypto.params.AsymmetricKeyParameter;
@@ -66,29 +67,31 @@ class ApplicationTests {
     @Test
     void jwtTest2() {
         try {
+            //第1部：授权服务-生成公私钥
             AsymmetricCipherKeyPair keyPair = SM2Util.generateKeyPairParameter();
             ECPrivateKeyParameters priKey = (ECPrivateKeyParameters) keyPair.getPrivate();
             ECPublicKeyParameters pubKey = (ECPublicKeyParameters) keyPair.getPublic();
 
+            //第2部：授权服务-暴露公钥
+            byte[] pubKeyX509Der = BCECUtil.convertECPublicKeyToX509(pubKey);
+            String pubKeyX509Pem = BCECUtil.convertECPublicKeyX509ToPEM(pubKeyX509Der);
+
+            //第3部：授权服务-用私钥加密
             byte[] SRC_DATA = "Hello tangming".getBytes();
-            byte[] WITH_ID = "auth-server".getBytes();
-
-            byte[] sign = SM2Util.sign(priKey, WITH_ID, SRC_DATA);
-            System.out.println("SM2 sign with withId result:\n" + ByteUtils.toHexString(sign));
-            byte[] rawSign = SM2Util.decodeDERSM2Sign(sign);
-            sign = SM2Util.encodeSM2SignToDER(rawSign);
-            System.out.println("SM2 sign with withId result:\n" + ByteUtils.toHexString(sign));
-            boolean flag = SM2Util.verify(pubKey, WITH_ID, SRC_DATA, sign);
-            if (!flag) {
-                Assert.fail("sign with withId verify failed");
-            }
-
-            sign = SM2Util.sign(priKey, SRC_DATA);
+            byte[] sign = SM2Util.sign(priKey, SRC_DATA);
             System.out.println("SM2 sign without withId result:\n" + ByteUtils.toHexString(sign));
-            flag = SM2Util.verify(pubKey, SRC_DATA, sign);
+
+            //第4部：资源服务-将公钥字符串转为对象
+            byte[] rs_pubKeyX509Der = BCECUtil.convertECPublicKeyPEMToX509(pubKeyX509Pem);
+            ECPublicKeyParameters rs_pubKey = BCECUtil.convertPublicKeyToParameters(BCECUtil.convertX509ToECPublicKey(rs_pubKeyX509Der));
+
+            //第5部：资源服务-用公钥验证
+            boolean flag = SM2Util.verify(rs_pubKey, SRC_DATA, sign);
             if (!flag) {
                 Assert.fail("sign without withId verify failed");
             }
+            else
+                System.out.println("success");
 
         } catch (Exception e) {
             System.out.printf("error");
